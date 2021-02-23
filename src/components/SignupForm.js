8c95faebd16101f034f1e9581d1b1b47251e07ef
gautam123kumar <kumargautam9518@gmail.com>
2021-02-23 12:30:40 +0530

    update

---
+++ src/components/SignupForm.js
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+function SignupForm(){
+    return(
+        <div>
+            <h1>guatam</h1>
+           
+            
+        </div>
+    )
+}
+
+export default SignupForm;
