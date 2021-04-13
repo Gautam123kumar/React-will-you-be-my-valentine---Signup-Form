dc6f690d678149923e901155c31e9e96e323acde
gautam123kumar <kumargautam9518@gmail.com>
2021-04-13 17:28:08 +0530

    new update

--- src/components/App.js
+++ src/components/App.js
@@ -1,81 +1,178 @@
 import React, { Component, useState } from "react";
-import '../styles/App.css';
+import InputBox from "./inputBox";
+import SelectInput from "./selectGender";
+import "../styles/App.css";
 
 const App = () => {
-  const [values,setValues] = useState({
+  const [fullName, setFullName] = useState({
     name: "",
     email: "",
-    gender: "male",
-    phonenumber: "",
+    gender: "Male",
+    number: "",
     password: "",
   });
-  const [submitted,setSubmitted] = useState(false);
-  const [valid,setValid] = useState(false);
 
-  const handleNameInputChange = (event) => {
-    setValues({...values,name: event.target.value});
+  const [inputError, setInputError] = useState({
+    inputName: false,
+    inputEmail: false,
+    inputGender: false,
+    inputNumber: false,
+    inputPass: false,
+  });
+  const [userName, setUserName] = useState("");
+
+  const inputChang = (event) => {
+    console.log(event.target.value);
+    console.log(event.target.name);
+
+    const { value, name } = event.target;
+
+    setFullName((prevalu) => {
+      return {
+        ...prevalu,
+        [name]: value,
+      };
+    });
   };
-
-  const handleEmailInputChange = (event) => {
-    setValues({...values,email: event.target.value});
-  };
-
-  const handleGenderInputChange = (event) => {
-    setValues({...values,gender: event.target.value});
-  };
-
-  const handlePhoneNumberInputChange = (event) => {
-    setValues({...values,phonenumber: event.target.value});
-  };
-
-  const handlePasswordInputChange = (event) => {
-    setValues({...values,password: event.target.value});
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if(values.name && values.email  && values.phonenumber && values.password && values.password.length > 5 && values.email.includes("@") && isNaN(values.name))
-    {
-      setValid(true);
+  const buttonClick = () => {
+    // event.preventDefault();
+    if (fullName.name === "") {
+      console.log("error");
+      setInputError((prevalue) => {
+        return { ...prevalue, inputName: true };
+      });
+    } else {
+      setInputError((prevalue) => {
+        return { ...prevalue, inputName: false };
+      });
     }
-    setSubmitted(true);
-  }
+    if (!fullName.email.includes("@") || fullName.email === "") {
+      console.log("error");
+      setInputError((prevalue) => {
+        return {
+          ...prevalue,
+          inputEmail: true,
+        };
+      });
+    } else {
+      setInputError((prevalue) => {
+        return {
+          ...prevalue,
+          inputEmail: false,
+        };
+      });
+      let sub = fullName.email.split("@");
+      setUserName("Hello" + " " + sub[0]);
+    }
+    if (fullName.gender === "") {
+      console.log("error");
+      setInputError((prevalue) => {
+        return { ...prevalue, inputGender: true };
+      });
+    } else {
+      setInputError((prevalue) => {
+        return { ...prevalue, inputGender: false };
+      });
+    }
+    if (fullName.number === "") {
+      console.log("error");
+      setInputError((prevalue) => {
+        return { ...prevalue, inputNumber: true };
+      });
+    } else {
+      setInputError((prevalue) => {
+        return { ...prevalue, inputNumber: false };
+      });
+    }
+    if (fullName.password.length < 6 || fullName.password === "") {
+      console.log("error");
+      setInputError((prevalue) => {
+        return { ...prevalue, inputPass: true };
+      });
+    } else {
+      setInputError((prevalue) => {
+        return { ...prevalue, inputPass: false };
+      });
+    }
+
+    // console.log(inputError.inputName);
+    // console.log(inputError.inputEmail);
+    // console.log(inputError.inputGender);
+    // console.log(inputError.inputNumber);
+    // console.log(inputError.inputPass);
+
+    // alert(
+    //   `Hello ${fullName.name}
+    //    ${fullName.email}
+    //    ${fullName.gender}
+    //    ${fullName.number}
+    //    ${fullName.password}`
+    // );
+  };
   return (
     <div id="main">
-      <form id="register-form" onSubmit={handleSubmit}>
-        {submitted && valid ? <div className="success-message">Hello {values.email.split('@')[0]}</div> : null}
-        {/* {submitted && valid ? resetValues() : null} */}
-        <input data-testid='name' className="form-field" onChange={handleNameInputChange} 
-        value={values.name}
-        type="text"
-        placeholder="Name"
-        name="Name"
-        required>
-        </input>
-        {submitted && !values.name ? <span>Name Error</span> : null}
-        {submitted && values.name && !isNaN(values.name) ? <span>Name is not alphanumeric</span> : null}
-        <input data-testid='email' className="form-field" value={values.email} onChange={handleEmailInputChange}
+      {!inputError.inputName &&
+      !inputError.inputEmail &&
+      !inputError.inputGender &&
+      !inputError.inputNumber &&
+      !inputError.inputPass ? (
+        <h1>{userName}</h1>
+      ) : (
+        ""
+      )}
+      <InputBox
+        data="name"
+        type="alphanumeric"
+        placeholder="Enter your Name"
+        defaultValue=""
+        name="name"
+        onChange={inputChang}
+        // value={fname}
+      />
+      {inputError.inputName ? <p>Name Error</p> : ""}
+      <InputBox
+        data="email"
         type="email"
-        placeholder="Email"
+        placeholder="Enter your Email"
+        defaultValue=""
         name="email"
-        required>
-        </input>
-        {submitted && !values.email ? <span>Email Error</span> : null}
-        {submitted && values.email && !values.email.includes("@") ? <span>Email must contain @</span> : null}
-        <select data-testid='gender' className="form-field" value={values.gender} onChange={handleGenderInputChange}>
-            <option defaultValue="male">male</option>
-            <option value="female">female</option>
-            <option value="others">others</option>
-        </select>
-        <input data-testid='phoneNumber' required className="form-field" value={values.phonenumber} onChange={handlePhoneNumberInputChange} type="number" placeholder="phone"></input>
-        {submitted && !values.phonenumber ? <span>Phone Number Error</span> : null}
-        <input data-testid='password' required id="password" type='password' className="form-field" value={values.password} onChange={handlePasswordInputChange} placeholder="password"></input>
-        {submitted && !values.password ? <span>Password Error</span> : null}
-        {submitted && values.password && document.getElementById('password').value.length < 6 ? <span>Password must contain atleast 6 letters</span> : null}
-        <button data-testid='submit' className="form-field" id="submit" placeholder="Submit">Submit</button>
-      </form>
+        onChange={inputChang}
+        // value={lname}
+      />
+      {inputError.inputEmail ? <p>Email must contain @</p> : ""}
+
+      <SelectInput data="gender" name="gender" onChange={inputChang} />
+      {inputError.inputGender ? <p>gender Error</p> : ""}
+
+      <InputBox
+        data="phoneNumber"
+        type="number"
+        placeholder="Enter your Phone Number"
+        defaultValue=""
+        name="number"
+        onChange={inputChang}
+        // value={lname}
+      />
+      {inputError.inputNumber ? <p>Phone Number Error</p> : ""}
+      <InputBox
+        data="password"
+        type="password"
+        placeholder="Enter password"
+        defaultValue=""
+        name="password"
+        onChange={inputChang}
+        // value={lname}
+      />
+      {inputError.inputPass ? (
+        <p> Password must contain atleast 6 letters</p>
+      ) : (
+        ""
+      )}
+      <button data-testid="submit" onClick={buttonClick}>
+        Submit
+      </button>
     </div>
-  )
-}
+  );
+};
 
 export default App;
