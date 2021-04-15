fc5d0887edf92b3e3567be152c6ed771002a471b
gautam123kumar <kumargautam9518@gmail.com>
2021-04-15 22:16:16 +0530

    update form

--- src/components/App.js
+++ src/components/App.js
@@ -1,176 +1,76 @@
 import React, { Component, useState } from "react";
-import InputBox from "./inputBox";
-import SelectInput from "./selectGender";
+import MainForm from "./MainForm/";
 import "../styles/App.css";
 
 const App = () => {
-  const [fullName, setFullName] = useState({
-    name: "",
-    email: "",
-    gender: "Male",
-    number: "",
-    password: "",
-  });
+  const [uName, setUname] = useState("");
+  const [eMail,setEmail] = useState("");
+  const [gender,setGender] = useState("Male");
+  const [phone,setPhone] = useState("");
+  const [password,setPassword] = useState("");
+  const [errorMsg,setErrormsg] = useState("");
 
-  const [inputError, setInputError] = useState({
-    inputName: false,
-    inputEmail: false,
-    inputGender: false,
-    inputNumber: false,
-    inputPass: false,
-  });
-  const [userName, setUserName] = useState("");
-
-  const inputChang = (event) => {
-    console.log(event.target.value);
-    console.log(event.target.name);
-
-    const { value, name } = event.target;
-
-    setFullName((prevalu) => {
-      return {
-        ...prevalu,
-        [name]: value,
-      };
-    });
+  const handleName = (event) => {
+    setUname(event.target.value);
   };
-  const buttonClick = () => {
-    // event.preventDefault();
-    if (fullName.name === "") {
-      console.log("error");
-      setInputError((prevalue) => {
-        return { ...prevalue, inputName: true };
-      });
-    } else {
-      setInputError((prevalue) => {
-        return { ...prevalue, inputName: false };
-      });
-    }
-    if (!fullName.email.includes("@") || fullName.email === "") {
-      console.log("error");
-      setInputError((prevalue) => {
-        return {
-          ...prevalue,
-          inputEmail: true,
-        };
-      });
-    } else {
-      setInputError((prevalue) => {
-        return {
-          ...prevalue,
-          inputEmail: false,
-        };
-      });
-      let sub = fullName.email.split("@");
-      setUserName("Hello" + " " + sub[0]);
-    }
-    if (fullName.gender === "") {
-      console.log("error");
-      setInputError((prevalue) => {
-        return { ...prevalue, inputGender: true };
-      });
-    } else {
-      setInputError((prevalue) => {
-        return { ...prevalue, inputGender: false };
-      });
-    }
-    if (fullName.number === "") {
-      console.log("error");
-      setInputError((prevalue) => {
-        return { ...prevalue, inputNumber: true };
-      });
-    } else {
-      setInputError((prevalue) => {
-        return { ...prevalue, inputNumber: false };
-      });
-    }
-    if (fullName.password.length < 6 || fullName.password === "") {
-      console.log("error");
-      setInputError((prevalue) => {
-        return { ...prevalue, inputPass: true };
-      });
-    } else {
-      setInputError((prevalue) => {
-        return { ...prevalue, inputPass: false };
-      });
-    }
 
-    // console.log(inputError.inputName);
-    // console.log(inputError.inputEmail);
-    // console.log(inputError.inputGender);
-    // console.log(inputError.inputNumber);
-    // console.log(inputError.inputPass);
-
-    // alert(
-    //   `Hello ${fullName.name}
-    //    ${fullName.email}
-    //    ${fullName.gender}
-    //    ${fullName.number}
-    //    ${fullName.password}`
-    // );
+  const handleEmail = (event) => {
+    setEmail(event.target.value);
   };
+
+  const handleGender = (event) => {
+    setGender(event.target.value);
+  };
+
+  const handlePhone = (event) => {
+    setPhone(event.target.value);
+  };
+
+  const handlePassword = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (e) =>{
+    console.log("submit button clicked");
+    // console.log(uName);
+    // console.log(eMail);
+    // console.log(gender);
+    // console.log(phone);
+    // console.log(password);
+    if(!uName || !eMail || !phone || !password ){
+      setErrormsg("All fields are mandatory");
+      e.preventDefault();
+      return;
+    }
+    else if(!uName.match(/^[0-9a-z]+$/)){
+      setErrormsg("Name is not alphanumeric");
+      e.preventDefault();
+      return;
+    }
+    else if(!eMail.match(/\S+@\S+\.\S+/)){
+      setErrormsg("Email must contain @");
+      e.preventDefault();
+      return;
+    }
+    else{
+      var name = eMail.substring(0, eMail.lastIndexOf("@"));
+      setErrormsg("Hello "+name);
+      e.preventDefault();
+    }
+    console.log("submitted")
+  }
   return (
     <div id="main">
-      {!inputError.inputName &&
-      !inputError.inputEmail &&
-      !inputError.inputGender &&
-      !inputError.inputNumber &&
-      !inputError.inputPass ? (
-        <h1>{userName}</h1>
-      ) : (
-        ""
-      )}
-      <InputBox
-        data="name"
-        type="alphanumeric"
-        placeholder="Enter your Name"
-        defaultValue=""
-        name="name"
-        onChange={inputChang}
-        // value={fname}
+      <MainForm
+        handleName={handleName}
+        handleEmail={handleEmail}
+        handleGender={handleGender}
+        defGender={gender}
+        handlePhone={handlePhone}
+        handlePassword={handlePassword}
+        handleSubmit={handleSubmit}
+        showError={errorMsg}
       />
-      {inputError.inputName ? <p>Name Error</p> : ""}
-      <InputBox
-        data="email"
-        type="email"
-        placeholder="Enter your Email"
-        defaultValue=""
-        name="email"
-        onChange={inputChang}
-        // value={lname}
-      />
-      {inputError.inputEmail ? <p>Email must contain @</p> : ""}
-
-      <SelectInput data="gender" name="gender" onChange={inputChang} />
-      {inputError.inputGender ? <p>gender Error</p> : ""}
-
-      <InputBox
-        data="phoneNumber"
-        type="number"
-        placeholder="Enter your Phone Number"
-        defaultValue=""
-        name="number"
-        onChange={inputChang}
-        // value={lname}
-      />
-      {inputError.inputNumber ? <p>Phone Number Error</p> : ""}
-      <InputBox
-        data="password"
-        type="password"
-        placeholder="Enter password"
-        defaultValue=""
-        name="password"
-        onChange={inputChang}
-        // value={lname}
-      />
-      {inputError.inputPass ? (
-        <p> Password must contain atleast 6 letters</p>
-      ) : (
-        ""
-      )}
-      <button data-testid="submit" onClick={buttonClick}>
-        Submit
-      </button>
     </div>
   );
 };
